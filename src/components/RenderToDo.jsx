37f32562d88d946040fe135f2c9862ccf8ad7faa
Murilo358 <murilobarbosa358@gmail.com>
2023-06-17 15:08:47 -0300

    adding see Description

--- src/components/RenderToDo.jsx
+++ src/components/RenderToDo.jsx
@@ -32,26 +32,34 @@ function RenderToDo({ todo, removeToDo, completeToDo }) {
             Ver detalhes
           </button>
 
-          <button
-            onClick={() => completeToDo(todo.id)}
-            className="complete todo-list-content__button"
-          >
-            <AiOutlineCheck />
-          </button>
+          <div>
+            <button
+              onClick={() => completeToDo(todo.id)}
+              className="complete todo-list-content__button"
+            >
+              <AiOutlineCheck />
+            </button>
 
-          <button
-            onClick={() => removeToDo(todo.id)}
-            className="delete todo-list-content__button"
-          >
-            <FaTrashAlt />
-          </button>
+            <button
+              onClick={() => removeToDo(todo.id)}
+              className="delete todo-list-content__button"
+            >
+              <FaTrashAlt />
+            </button>
+          </div>
         </div>
       </div>
       {showDescription && (
-        <div
-          className="todo-description"
-          dangerouslySetInnerHTML={{ __html: todo.description }}
-        />
+        <div className="todo-details">
+          <hr />
+          <p>Categoria: {todo.category}</p>
+
+          <p>Descrição:</p>
+          <div
+            className="todo-description"
+            dangerouslySetInnerHTML={{ __html: todo.description }}
+          />
+        </div>
       )}
     </div>
   );
